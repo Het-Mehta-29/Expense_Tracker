e7614ac676c219c1e775e4ad8b1dc40b620164fc
Het-Mehta-29 <hetmehta040506@gmail.comgit config --global user.email hetmehta040506@gmail.com>
2025-08-13 17:06:39 +0530

    Fixed API fetch calls and updated useTransactions hook

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ import dotenv from 'dotenv';
 import { initdb } from './config/db.js';
 import ratelimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
+import job from "./config/cron.js";
 
 dotenv.config();
+if (process.env.NODE_ENV === "production") job.start();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
